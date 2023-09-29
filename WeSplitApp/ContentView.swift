c76760f27f3a5569d694e783a05f6b9e52c16bcc
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 15:53:29 +0530

    First Commit

--- WeSplitApp/ContentView.swift
+++ WeSplitApp/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                
+            }
+            
+            .navigationTitle("We Split App")
         }
-        .padding()
     }
 }
 
